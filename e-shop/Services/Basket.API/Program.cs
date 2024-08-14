5b0ddffa5dd47c6485d282920ee13b0187a0c4f3
alex-toy <alexei.80@hotmail.fr>
2024-08-14 10:50:07 +0200

    caching

--- e-shop/Services/Basket.API/Program.cs
+++ e-shop/Services/Basket.API/Program.cs
@@ -1,4 +1,7 @@
 using BuildingBlocs.Behaviors;
+using BuildingBlocs.Exceptions.Handlers;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,14 +20,32 @@ builder.Services.AddMediatR(config =>
 
 builder.Services.AddValidatorsFromAssembly(assembly);
 
+string postgresStringConnection = builder.Configuration.GetConnectionString("Database")!;
+string redisStringConnection = builder.Configuration.GetConnectionString("Redis")!;
+
 builder.Services.AddMarten(options =>
 {
-    string connectionString = builder.Configuration.GetConnectionString("Database")!;
-    options.Connection(connectionString);
+    options.Connection(postgresStringConnection);
     options.Schema.For<ShoppingCart>().Identity(x => x.UserName);
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = redisStringConnection;
+    //options.InstanceName = "Basket";
+});
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(postgresStringConnection)
+    .AddRedis(redisStringConnection);
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+
+
 
 
 
@@ -33,5 +54,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.MapCarter();
 app.UseExceptionHandler(options => { });
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    }
+);
 
 app.Run();
