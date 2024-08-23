abd4e636fbde6db084ec95bd46bd9202187d74ee
alex-toy <alexei.80@hotmail.fr>
2024-08-23 11:01:29 +0200

    docker discount

--- e-shop/Services/Discount.Grpc/Program.cs
+++ e-shop/Services/Discount.Grpc/Program.cs
@@ -1,14 +1,25 @@
+using Discount.Grpc.Data;
 using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
 
-var app = builder.Build();
+string connectionString = builder.Configuration.GetConnectionString("Database")!;
+builder.Services.AddDbContext<DiscountContext>(opts => opts.UseSqlite(connectionString));
 
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+
+
+
+
+WebApplication app = builder.Build();
+
+app.UseMigration();
+
+app.MapGrpcService<DiscountService>();
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
